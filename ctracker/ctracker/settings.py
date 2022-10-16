7d83f91934c16858921bba504f14f86fa422e566
rameez012001 <73241967+rameez012001@users.noreply.github.com>
2022-10-16 19:04:28 +0530

    Update settings.py

--- ctracker/ctracker/settings.py
+++ ctracker/ctracker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ctapp.apps.CtappConfig',
+   #'ctapp.apps.CtappConfig',
 ]
 
 MIDDLEWARE = [
